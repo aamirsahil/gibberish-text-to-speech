ab07a75889614a1ce590cd392dec88cf098abaf0
Aamir Sahil <notifysahil@gmail.com>
2025-04-14 08:56:03 +0530

    gibberish engine modified with divide text, find audio and modify audio functions

--- src/gibberish_tts/gibberish_tts.py
+++ src/gibberish_tts/gibberish_tts.py
@@ -1,8 +1,22 @@
+from typing import List, Dict
+
 class TTSEngine():
     def __init__(self):
         pass
 
     def load(self, text : str) -> None:
+        text_pieces = self.divideToPieces(text)
+        audio_pieces = self.findAudio(text_pieces)
+        audio_pieces = self.modifyAudio(audio_pieces)
+
+    def divideToPieces(self, text : str) -> List[str]:
+        text_pieces = text.split()
+        return text_pieces
+    
+    def findAudio(self, text_pieces : List[str]) -> List[Dict]:
+        pass
+
+    def modifyAudio(self, audio_pieces : List[Dict]) -> List[Dict]:
         pass
 
     def play(self) -> None:
