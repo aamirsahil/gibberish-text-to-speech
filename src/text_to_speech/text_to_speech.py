6275b72ed17123f0d6f3f9497d3b4795717c2d79
Aamir Sahil <notifysahil@gmail.com>
2025-04-14 08:34:48 +0530

    class for tts added

---
+++ src/text_to_speech/text_to_speech.py
@@ -0,0 +1,11 @@
+from pytts import TTSEngine
+
+class TextToSpeechV1:
+    def __init__(self):
+        self.engine = TTSEngine()
+
+    def load_speech(self, text : str) -> None:
+        self.engine.load(text)
+
+    def play_speech(self) -> None:
+        self.engine.play()
